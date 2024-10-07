e5e8239bdb62d687ca6dd8d9ca09fadcb6699db7
Leonardo Ferrari Fraga <Leonardo_.L@hotmail.com>
2024-10-07 12:04:17 -0300

    Add Extension methods for Vector3 and Color

---
+++ Assets/_Game/Scripts/Extension Methods/ColorExtensions.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions
+{
+    public static Color Add(this ref Color color, float value) => color = new Color(color.r + value, color.g + value, color.b + value);
+}
---
+++ Assets/_Game/Scripts/Extension Methods/Vector3Extensions.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public static class Vector3Extensions
+{
+    public static Vector3 Add(this ref Vector3 vector, float value) => vector = new Vector3(vector.x + value, vector.y + value, vector.z + value);
+}
